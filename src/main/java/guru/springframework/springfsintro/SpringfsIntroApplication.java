570229d4f5eb3ef39089abd4947911f8cf08515f
John Thompson <john@springframework.guru>
2025-07-11 11:45:31 -0400

    initial project add

---
+++ src/main/java/guru/springframework/springfsintro/SpringfsIntroApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springfsintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfsIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringfsIntroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/springfsintro/SpringfsIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springfsintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfsIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
